106e27e47c4d0fef596ed27abbaf09b6a9ea95a4
Katrin Baumgaertner <katrin.baumgaertner@pluto.uni-freiburg.de>
2018-03-10 13:15:57 +0100

    filenames as params

--- R/single.class.importance.R
+++ R/single.class.importance.R
@@ -1,38 +1,35 @@
-#' single class importance
-#' 
-#' calculates importance of single class from xgb.model
+#' Single Class Importance
 #'
-# @param savefolder
+#' Calculate the importance of a single class from a given xgb Model.
+#'
+#' @param model_fn name of the file storing the xgb model
+#'  (see \link{train.model})
+#' @param feature_names_fn name of the file storing the feature names
+#'  (see \link{import.data})
 #' @param x class for which importance should be calculated
 #' @import xgboost
-#' @import data.table
+#' @importFrom data.table fread
 #' @export
 
-single.class.importance <- function(#savefolder,
-                                    model = NA,
-                                    x)  {
-  
-  if(is.na(model))  {
-    bst <- xgb.load("model/xgb.model")
-  } else {
-    bst <- xgb.load(model)
-  }
-  label <- fread("feature.names", header = F)$V1
-  
+single.class.importance <- function(model_fn, feature_names_fn, x)  {
+
+  bst <- xgb.load(model_fn)
+  label <- fread(feature_names_fn, header = F)$V1
+
   dt <- xgb.model.dt.tree(label, bst)
   num.class <- get.parameter("num_class")
   nrounds <- (max(dt[,1]) + 1)/num.class
-  
+
   #creating list with corresponding tree indices
   xlist <- list()
   for(i in 1:nrounds){#i is class
     xlist[i] <- x-1+(i-1)*num.class
   }
-  
+
   #getting new data table with corresponding indices
   dt_new <- dt[as.matrix(dt[,1]) %in% xlist,]
   dt_new[,1] <- (dt_new[,1]-dt_new[,1]%%num.class)*2/num.class + dt_new[,1]%%num.class
-  
+
   #gain
   gain <- c()
   gain[label] <- 0
@@ -41,9 +38,9 @@ single.class.importance <- function(#savefolder,
       gain[c(dt_new$Feature[i])] <- gain[c(dt_new$Feature[i])] + dt_new$Quality[i]
     }
   }
-  
+
   Gain <- gain/sum(gain)
-  
+
   #frequency
   freq <- c()
   freq[label] <- 0
@@ -51,7 +48,7 @@ single.class.importance <- function(#savefolder,
     freq[i] <- sum(dt_new$Feature == label[i])
   }
   Frequency <- freq/sum(freq)
-  
+
   #cover
   cover <- c()
   cover[label] <- 0
@@ -61,7 +58,7 @@ single.class.importance <- function(#savefolder,
     }
   }
   Cover <- cover/sum(cover)
-  
+
   importance <- cbind(Gain, Cover, Frequency)
   importance <- importance[order(-Gain),]
   return(importance)
