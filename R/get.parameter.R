7ec0d1cd9ac669aa211241f3c1b4b5568dbf7194
Katrin Baumgaertner <katrin.baumgaertner@pluto.uni-freiburg.de>
2018-03-10 12:10:51 +0100

    get/set parameter methods in one file, moved default params from train.model to parameter.R

--- R/get.parameter.R
+++ R/get.parameter.R
@@ -1,12 +1,3 @@
-#' get parameter
-#' 
-#' opens parameter file and returns requested parameter
-#' 
-#' @param parameter
-#' @import data.table
-#' @export
 
-get.parameter <- function(parameter)  {
-  prm <- fread("parameter")
-  return(as.numeric(as.list(prm)[parameter]))
-}
+
+
--- R/train.model.R
+++ R/train.model.R
@@ -1,5 +1,5 @@
 #' train model
-#' 
+#'
 #' trains xgboost model
 #'
 #' @param savefolder
@@ -8,45 +8,31 @@
 #' @import data.table
 #' @export
 #ToDo: Parameter, test.matrix
-train.model <- function(savefolder,
-                        nrounds = 20) {
-  setwd(savefodler)
-  dir.create("model")
-  parameter <- as.list(fread(parameter))
-  train.matrix <- xgb.DMatrix("train.matrix.data")
-  test.matrix <- xgb.DMatrix("test.matrix.data")
+train.model <- function(data_dir, output_dir, params, num_class, nrounds = 20) {
+
+  if(!dir.exists(output_dir)) {
+    message(paste("Creating output directory", output_dir))
+    dir.create(output_dir, showWarnings = F, recursive = T)
+  }
+
+  train.matrix <- xgb.DMatrix(paste(data_dir, "train.matrix.data", sep="/"))
+  test.matrix  <- xgb.DMatrix(paste(data_dir, "test.matrix.data", sep="/"))
+
   watchlist <- c(train = train.matrix, test = test.matrix)
-  num.class <- get.num.class(train.matrix, test.matrix)
-  label <- fread("feature.names", header = F)$V1
-  
-  if(!("eval_metric" %in% parameter)) {
-    parameter <- c(parameter, eval_metric = 'merror')
-  }
-  if(!("objective" %in% parameter)) {
-    parameter <- c(parameter, objective = 'multi:softmax')
-  }
+  label <- fread("feature.names", header = F)[[1]]
+
   if(!("num_class" %in% parameter)) {
     parameter <- c(parameter, num_class = num.class)
   }
-  if(!("nthread" %in% parameter)) {
-    parameter <- c(parameter, nthread = 0)
-  }
-  if(!("eta" %in% parameter)) {
-    parameter <- c(parameter, eta = 0.3)
-  }
-  if(!("max_depth" %in% parameter)) {
-    parameter <- c(parameter, max_depth = 6)
-  }
-  
-  bst <- xgb.train(data = train.matrix, 
-                   watchlist = watchlist, 
-                   params = parameter,
-                   nrounds = nrounds
-                   )
+
+  bst <- xgb.train(data = train.matrix,
+                   watchlist = watchlist,
+                   params = params,
+                   nrounds = nrounds)
   ##save
-  xgb.save(bst, paste("model/xgb.model", sep=""))
-  xgb.dump(bst, paste("model/xgb.dump.model", sep=""), with_stats = TRUE)
+  xgb.save(bst, paste(output_dir, "xgb.model", sep="/"))
+  xgb.dump(bst, paste(output_dir, "xgb.dump.model", sep="/"), with_stats = TRUE)
   imp <- xgb.importance(model = bst, feature_names = label)
-  write.csv(imp, "model/importance")
-  
+  write.csv(imp, paste(output_dir, "importance", sep="/"))
+
   }
